a7abf2b5dab482e6fd1f1071f8d68d434a62b390
Niguel Gunther <niguelgunther@gmail.com>
2020-06-28 14:25:20 -0300

    bootstrap style etc

--- health/user_example/forms.py
+++ health/user_example/forms.py
@@ -3,11 +3,23 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+# aangepast UserCreationForm met bootstrap style
 
 class CreateUserForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
+    username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password1=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    
+# volgorde van de reg list    
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+        '''
+       widgets = {
+          'username': form.TextInput(attrs={'class': 'form-control'}),
+        }
+        '''
