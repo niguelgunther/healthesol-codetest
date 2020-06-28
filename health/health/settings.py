c0d3812647b1cd1e5008de08592cbec6f280610d
Niguel Gunther <niguelgunther@gmail.com>
2020-06-28 02:07:08 -0300

    edits

--- health/health/settings.py
+++ health/health/settings.py
@@ -31,10 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # my apps
-    'auth',
-
-    # django apps
+    'user_example',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
