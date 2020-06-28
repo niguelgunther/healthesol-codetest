d62830a14e7afae36f8b36e8e77536b792f29fc3
Niguel Gunther <niguelgunther@gmail.com>
2020-06-28 02:09:53 -0300

    edits1

--- health/user_example/views.py
+++ health/user_example/views.py
@@ -9,18 +9,16 @@ def index(request):
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-        
-    if form.is_valid():
-        form.save()
-        username = form.cleaned_data['username']
-        password = form.cleaned_data['password']
-        user = authenticate(username=username, password=password)
-        login(request, user)
-        return redirect('index')
 
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
     else:
         form = UserCreationForm()
 
     context = {'form' : form}
-    return render(request, 'registration/register.html', context)    
-
+    return render(request, 'registration/register.html', context)   
