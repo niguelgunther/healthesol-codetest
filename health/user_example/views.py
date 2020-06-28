97bed6aeebaa14c3befd371b0516846e1c19c36d
Niguel Gunther <niguelgunther@gmail.com>
2020-06-28 02:42:14 -0300

    registration fields

--- health/user_example/views.py
+++ health/user_example/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 
+from .forms import CreateUserForm
+
 # Create your views here.
 def index(request):
     return render(request, 'user_example/index.html')
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
 
         if form.is_valid():
             form.save()
@@ -18,7 +20,7 @@ def register(request):
             login(request, user)
             return redirect('index')
     else:
-        form = UserCreationForm()
+        form = CreateUserForm()
 
     context = {'form' : form}
     return render(request, 'registration/register.html', context)   
